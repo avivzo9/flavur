c244e84af250b1924ed22da6e4f3b73ea0130bdf
Aviv Zohar <avivzo9@gmail.com>
2022-01-13 12:13:21 +0200

    Map - Icons changes & user location added

--- src/features/map/screens/Map.screen.js
+++ src/features/map/screens/Map.screen.js
@@ -3,13 +3,16 @@ import { StyleSheet } from "react-native";
 import MapView, { Marker } from 'react-native-maps';
 import { LocationContext } from "../../../services/location/location.context";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { AppConfigContext } from "../../../services/appConfig/appConfig.context";
 import Loader from "../../Loader";
 import Search from "../../restaurants/cmps/Search.cmp";
 import MapCallout from "../cmps/map.callout";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function MapScreen({ route, navigation }) {
     const { location } = useContext(LocationContext)
     const { restaurants, restaurantLoading } = useContext(RestaurantsContext)
+    const { isDarkMode } = useContext(AppConfigContext)
     const { viewport } = location.geometry
 
     const [latDelta, setLatDelta] = useState(null)
@@ -29,12 +32,19 @@ export default function MapScreen({ route, navigation }) {
     return (
         <>
             <Search routeName={route.name} />
-            <MapView style={styles.map} region={{ latitude: lat, longitude: lng, latitudeDelta: latDelta, longitudeDelta: 0.05 }}>
+            <MapView
+                style={styles.map}
+                region={{ latitude: lat, longitude: lng, latitudeDelta: latDelta, longitudeDelta: 0.05 }}
+                showsUserLocation={true}
+                followsUserLocation={false}
+                userInterfaceStyle={isDarkMode ? 'dark' : 'light'}
+            >
                 {restaurants.map((rest, idx) => <Marker
                     key={rest.name + idx}
                     title={rest.name}
                     coordinate={{ latitude: rest.geometry.location.lat, longitude: rest.geometry.location.lng }}
                 >
+                    <Ionicons name='location-sharp' size={45} color="tomato" />
                     <MapView.Callout onPress={() => navigation.navigate('RestaurantDetails', { restaurant: rest })}>
                         <MapCallout restaurant={rest} />
                     </MapView.Callout>
